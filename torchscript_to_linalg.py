8687fdf79551dea03989c3d7088c018c03bb4ae3
Ramiro Leal-Cavazos <ramiroleal050@gmail.com>
2022-10-25 21:33:01 +0000

    More changes

--- torchscript_to_linalg.py
+++ torchscript_to_linalg.py
@@ -7,58 +7,57 @@ To run the example, make sure the following are in your PYTHONPATH:
 then, simply call `python torchscript_to_linalg.py`.
 """
 
+import argparse
+
 import torch
 import torchvision.models as models
 import torch_mlir
 import warnings
 from torch_mlir_e2e_test.linalg_on_tensors_backends.refbackend \
     import RefBackendLinalgOnTensorsBackend
-from torch_mlir_e2e_test.tosa_backends.linalg_on_tensors import LinalgOnTensorsTosaBackend
 
-def _print_title(title: str):
-    print()
-    print(title)
-    print('-' * len(title))
+OUTPUT_TYPES = ['raw', 'torch', 'linalg-on-tensors']
+parser = argparse.ArgumentParser()
+parser.add_argument("--output-type", type=str, choices=OUTPUT_TYPES, default=None)
+parser.add_argument("--check-with-eager", action="store_true")
+args = parser.parse_args()
 
-example_inputs = [torch.rand((3,4,5,6)), torch.randint(0, 1, (4,2)), torch.tensor([0, 1])]
-placeholder_inputs = [torch_mlir.TensorPlaceholder.like(x, dynamic_axes=[]) for x in example_inputs]
+example_inputs = [torch.tensor([1]), torch.tensor([1])]
+#placeholder_inputs = [torch_mlir.TensorPlaceholder.like(x, dynamic_axes=list(range(len(x.shape)))) for x in example_inputs]
+placeholder_inputs = example_inputs
+
+def foo(x):
+    return (x,)
 
 class MyModule(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, a, b, c):
-        return torch.ops.aten.index(a, [b, None, c])
-
+    def forward(self, x, y):
+        return torch.add(x, y)
 
 module = MyModule()
 script_module = torch.jit.script(module, example_inputs)
-_print_title("TorchScript")
-print(script_module.graph)
+if args.output_type is None:
+    print(f"// OuputType: torchscript")
+    print(script_module.graph)
 
-torch_module = torch_mlir.compile(script_module, placeholder_inputs,
-                                  output_type=torch_mlir.OutputType.RAW)
-_print_title("Torch-MLIR")
-print(torch_module)
+for output_type in OUTPUT_TYPES:
+    torch_mlir_module = torch_mlir.compile(script_module, placeholder_inputs,
+                                           output_type=output_type)
+    if args.output_type is None or output_type == args.output_type:
+        print(f"// OuputType: {output_type}")
+        print(torch_mlir_module)
 
-torch_module = torch_mlir.compile(script_module, placeholder_inputs,
-                                  output_type=torch_mlir.OutputType.TORCH)
-_print_title("Torch-MLIR-backend")
-print(torch_module)
+if args.check_with_eager:
+    backend = RefBackendLinalgOnTensorsBackend()
+    compiled = backend.compile(torch_mlir_module)
+    jit_module = backend.load(compiled)
 
-linalg_module = torch_mlir.compile(script_module, placeholder_inputs,
-                                   output_type=torch_mlir.OutputType.LINALG_ON_TENSORS)
+    print("Running Compiled Graph")
+    print('Expected output:')
+    print(script_module.forward(*example_inputs))
+    print('Output from compiled MLIR:')
+    numpy_inputs = list(map(lambda x: x.numpy(), example_inputs))
+    print(torch.tensor(jit_module.forward(*numpy_inputs)))
 
-_print_title("Linalg-MLIR")
-print(linalg_module)
-
-backend = RefBackendLinalgOnTensorsBackend()
-compiled = backend.compile(linalg_module)
-jit_module = backend.load(compiled)
-
-_print_title("Running Compiled Graph")
-print('Expected output:')
-print(script_module.forward(*example_inputs))
-print('Output from compiled MLIR:')
-numpy_inputs = list(map(lambda x: x.numpy(), example_inputs))
-print(torch.tensor(jit_module.forward(*numpy_inputs)))
